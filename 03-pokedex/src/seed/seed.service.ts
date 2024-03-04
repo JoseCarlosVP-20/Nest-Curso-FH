8b6c398db9131411e231d560f77144e2dbc9e106
José Carlos Villalba P <jose.villalba@uhispano.ac.cr>
2024-03-03 23:14:06 -0600

    feat: 🗃️ batch Pokemon insertion

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Pokemon } from '../pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+
+  constructor(@InjectModel(Pokemon.name) private readonly pokemonModel) {}
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({ name, url }) => {
+    for (const { name, url } of data.results) {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
-      console.log({ no, name });
-    });
-    return data.results;
+      const pokemon = await this.pokemonModel.create({ no, name });
+    }
+    return 'Seed executed successfully!';
   }
 }
